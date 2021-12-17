42d9ad0dde65257a1216af4d0daa8ecb91b6d8b0
takemoto222 <omochi9797@icloud.com>
2021-12-17 17:42:55 +0900

    1217_5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,17 +35,23 @@ class PostController extends Controller
     public function item_create(Request $request)
     {
         $request->all();
-        $image_file = $request->image;
-        $image_file = $request->image2; //追加
-        $image_file = $request->file('image')->store('public/image');
+        $image_file = $request->image;   //image_fileという変数に$request->imageの中身を追加する
+        $image_file = $request->file('image')->store('public/image');  //publicのimageに保存する
         $image_file = str_replace('public/image/', '', $image_file);
         $image = new Post;
-        $image = $image_file;
+        $image = $image_file; //$imageにファイル名を入れる
+        //二枚目
+        $image2_file = $request->image2;
+        $image2_file = $request->file('image2')->store('public/image');
+        $image2_file = str_replace('public/image/', '', $image2_file);
+        $image2 = new Post;
+        $image2 = $image2_file;
+        //二枚目追加ここまで
         $param = [
             'name' => $request->name,
             'content' => $request->content,
             'image' => $image,
-            'image2' => $image, //追加
+            'image2' => $image2,
             /*'user_id' => $request->user_id,*/
         ];
         DB::insert('insert into posts(name, content, image, image) values(:name, :content, :image, :image2)', $param); //追加
@@ -55,12 +61,14 @@ class PostController extends Controller
     public function item_image(Request $request)
     {
         $image = $request->image;
-        $image = $request->image2; //追加
+        $image2 = $request->image2; //追加
         $image = $request->file('image', 'image2')->store('public/image'); //追加
         $image = str_replace('public/image/', '', $image);
         $image = new Post;
         $image->file = $image;
+        $image2->file = $image2; //追加
 
         $image->save();
+        $image2->save(); //追加
     }
 }
